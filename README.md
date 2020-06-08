643863423a6e8950c5e36de9c0db09a838e71c7a
Victor Albertos Gil <me@victoralbertos.com>
2020-06-08 13:04:37 +0200

    initial public commit

---
+++ README.md
@@ -0,0 +1,50 @@
+An Android library that allows, when using the BottomNavigationBar, to consume the multiple back stack workaround provided on [Google Samples](https://github.com/android/architecture-components-samples/blob/master/NavigationAdvancedSample/app/src/main/java/com/example/android/navigationadvancedsample/NavigationExtensions.kt) with one single navigation graph per application.
+
+### When to use this library?
+When using the BottomNavigationBar, the Navigation component, multiple back stack workaround and one single nav graph per app.
+
+### Why would I want to use one single navigation graph per app?
+You may don't want to, but based on our experience, when using Navigation component in conjunction with [Safe Args](https://developer.android.com/topic/libraries/architecture/navigation/navigation-pass-data#Safe-args) plugin, using nested graphs will make you repeat yourself like a parrot.
+
+<details>
+  <summary>See these limitations</summary>
+  
+  1. The arguments declared in the destination of another graph need to be copy/paste into the action that points to that destination. This leads to huge duplications of code and also opens the door to dangerous situations: if you add a new argument to the destination and forget to update all the actions that point to that destination (and there can be a lot of screens), the app will crash at runtime and the SafeArgs plugin will not be longer "safe".
+  2. Global actions can not be shared between graphs, which means that any screen accessing another screen that is not declared in the same graph requires to rewrite its associated action. This issue, in conjunction with the previous one, guarantees massive code duplication.
+  3. When calling a graph from another graph is not possible to select the entry point (the screen that we want to launch), we can only use the one annotated as such in the destination graph (this is the reason that we can't split the graphs with a Gradle module scope).
+</details>
+
+### How to use it?
+
+Add to top level gradle.build file
+```
+allprojects {
+    repositories {
+        maven { url "https://jitpack.io" }
+    }
+}
+```
+
+Add to app module gradle.build file
+```
+dependencies {
+    compile 'com.github.cookpad:BottomNavWatson:0.0.1'
+}
+```
+
+The API is pretty much the same that is provided in Google Samples: an extension function `BottomNavigationView.setupWithNavController` which requires to supply a few parameters to do the setup. You can go to our sample to see a showcase.
+
+```kotlin
+fun BottomNavigationView.setupWithNavController(
+    @NavigationRes graphResId: Int,
+    activity: AppCompatActivity,
+    selectedTabId: Int,
+    enabledTabs: List<Int>,
+    containerId: Int,
+    destinationChangedListener: NavController.OnDestinationChangedListener? = null,
+    navigationItemReselectedListener: BottomNavigationView.OnNavigationItemReselectedListener? = null
+)
+```
+
+### Disclaimer
+This library is a workaround within the workaround provided in Google Samples, so it may have unexpected side effects. For more info checks the [docs attached in the sources](https://github.com/cookpad/BottomNavWatson/blob/master/bottom-nav-watson/src/main/kotlin/bottom_nav_watson/BottomNavWatson.kt).
---
+++ bottom-nav-watson/src/main/kotlin/bottom_nav_watson/BottomNavWatson.kt
@@ -0,0 +1,287 @@
+package bottom_nav_watson
+
+import android.content.Intent
+import android.util.SparseArray
+import androidx.annotation.NavigationRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.util.forEach
+import androidx.core.util.set
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+/**
+ * NavController setup for BottomNavigationView with multiple back stacks and one single navigation graph.
+ * This implementations is an adaptation from the one that Google provides in its architecture-components-samples
+ * that differs from it only in that this one allows to use a single nav graph for the entire app.
+ * Original source: https://github.com/android/architecture-components-samples/blob/master/NavigationAdvancedSample
+ * /app/src/main/java/com/example/android/navigationadvancedsample/NavigationExtensions.kt
+ *
+ * selectedTabId parameter will only be taken into consideration when there isn't a deep link, otherwise it will set
+ * the first tab as the selected one. This is required to avoid messing up the back stack, which seems to not work
+ * properly when the tab is changed during the navController setup + presence of deep link.
+ */
+fun BottomNavigationView.setupWithNavController(
+    @NavigationRes graphResId: Int,
+    activity: AppCompatActivity,
+    selectedTabId: Int,
+    enabledTabs: List<Int>,
+    containerId: Int,
+    destinationChangedListener: NavController.OnDestinationChangedListener? = null,
+    navigationItemReselectedListener: BottomNavigationView.OnNavigationItemReselectedListener? = null
+): LiveData<NavController> {
+    val fragmentManager = activity.supportFragmentManager
+
+    // Map of tags
+    val graphIdToTagMap = SparseArray<String>()
+    // Result. Mutable live data with the selected controlled
+    val selectedNavController = MutableLiveData<NavController>()
+
+    var firstFragmentGraphId = 0
+
+    // First create a NavHostFragment for each NavGraph ID
+    enabledTabs.forEachIndexed { index, tabId ->
+        val fragmentTag = getFragmentTag(index)
+
+        // Find or create the Navigation host fragment
+        val navHostFragment = activity.obtainNavHostFragment(
+            graphResId,
+            tabId,
+            fragmentManager,
+            fragmentTag,
+            containerId
+        )
+
+        if (index == 0) {
+            firstFragmentGraphId = tabId
+        }
+
+        // Save to the map
+        graphIdToTagMap[tabId] = fragmentTag
+
+        // Attach or detach nav host fragment depending on whether it's the selected item.
+        if (this.selectedItemId == tabId) {
+            // Update livedata with the selected graph
+            selectedNavController.value = navHostFragment.navController.apply {
+                destinationChangedListener?.let { addOnDestinationChangedListener(it) }
+            }
+            attachNavHostFragment(
+                fragmentManager,
+                navHostFragment,
+                index == 0
+            )
+        } else {
+            detachNavHostFragment(fragmentManager, navHostFragment)
+        }
+    }
+
+    // Now connect selecting an item with swapping Fragments
+    var selectedItemTag = graphIdToTagMap[this.selectedItemId]
+    val firstFragmentTag = graphIdToTagMap[firstFragmentGraphId]
+    var isOnFirstFragment = selectedItemTag == firstFragmentTag
+
+    // When a navigation item is selected
+    setOnNavigationItemSelectedListener { item ->
+        // Don't do anything if the state is state has already been saved.
+        if (fragmentManager.isStateSaved) {
+            false
+        } else {
+            val newlySelectedItemTag = graphIdToTagMap[item.itemId]
+            if (selectedItemTag != newlySelectedItemTag) {
+                // Pop everything above the first fragment (the "fixed start destination")
+                fragmentManager.popBackStack(
+                    firstFragmentTag,
+                    FragmentManager.POP_BACK_STACK_INCLUSIVE
+                )
+                val selectedFragment = fragmentManager.findFragmentByTag(newlySelectedItemTag)
+                        as NavHostFragment
+
+                // Exclude the first fragment tag because it's always in the back stack.
+                if (firstFragmentTag != newlySelectedItemTag) {
+                    // Commit a transaction that cleans the back stack and adds the first fragment
+                    // to it, creating the fixed started destination.
+                    fragmentManager.beginTransaction()
+                        .attach(selectedFragment)
+                        .setPrimaryNavigationFragment(selectedFragment)
+                        .apply {
+                            // Detach all other Fragments
+                            graphIdToTagMap.forEach { _, fragmentTagIter ->
+                                if (fragmentTagIter != newlySelectedItemTag) {
+                                    detach(fragmentManager.findFragmentByTag(firstFragmentTag)!!)
+                                }
+                            }
+                        }
+                        .addToBackStack(firstFragmentTag)
+                        .setReorderingAllowed(true)
+                        .commit()
+                }
+
+                destinationChangedListener?.let {
+                    selectedNavController.value?.removeOnDestinationChangedListener(destinationChangedListener)
+                }
+                selectedItemTag = newlySelectedItemTag
+                isOnFirstFragment = selectedItemTag == firstFragmentTag
+                selectedNavController.value = selectedFragment.navController.apply {
+                    destinationChangedListener?.let {
+                        addOnDestinationChangedListener(destinationChangedListener)
+                    }
+                }
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    // Optional: on item reselected, pop back stack to the destination of the graph
+    setupItemReselected(navigationItemReselectedListener, graphIdToTagMap, fragmentManager)
+
+    val isThereDeepLink = addDestinationFragmentFromDeepLinkIfNeeded(
+        graphResId,
+        activity,
+        enabledTabs,
+        fragmentManager,
+        containerId
+    )
+    // Only select the specified tab if there wasn't a deep link -otherwise the back-stack is messed up.
+    if (!isThereDeepLink) {
+        selectedItemId = selectedTabId
+    }
+
+    // Finally, ensure that we update our BottomNavigationView when the back stack changes
+    fragmentManager.addOnBackStackChangedListener {
+        if (!isOnFirstFragment && !fragmentManager.isOnBackStack(firstFragmentTag)) {
+            this.selectedItemId = firstFragmentGraphId
+        }
+
+        // Reset the graph if the currentDestination is not valid (happens when the back
+        // stack is popped after using the back button).
+        selectedNavController.value?.let { controller ->
+            if (controller.currentDestination == null) {
+                controller.navigate(controller.graph.id)
+            }
+        }
+    }
+
+    return selectedNavController
+}
+
+private fun addDestinationFragmentFromDeepLinkIfNeeded(
+    @NavigationRes graphResId: Int,
+    activity: AppCompatActivity,
+    enabledTabs: List<Int>,
+    fragmentManager: FragmentManager,
+    containerId: Int
+): Boolean {
+    // Add the target screen from the deep link to the first tab/stack
+    val index = 0
+    val firstTabId = enabledTabs[index]
+    val fragmentTag = getFragmentTag(index)
+
+    // Find or create the Navigation host fragment
+    val navHostFragment = activity.obtainNavHostFragment(
+        graphResId,
+        firstTabId,
+        fragmentManager,
+        fragmentTag,
+        containerId
+    )
+
+    return navHostFragment.navController.handleDeepLink(activity.intent)
+}
+
+private fun BottomNavigationView.setupItemReselected(
+    navigationItemReselectedListener: BottomNavigationView.OnNavigationItemReselectedListener?,
+    graphIdToTagMap: SparseArray<String>,
+    fragmentManager: FragmentManager
+) {
+    setOnNavigationItemReselectedListener { item ->
+        navigationItemReselectedListener?.onNavigationItemReselected(item)
+
+        val newlySelectedItemTag = graphIdToTagMap[item.itemId]
+        val selectedFragment = fragmentManager.findFragmentByTag(newlySelectedItemTag)
+                as NavHostFragment
+        val navController = selectedFragment.navController
+        // Pop the back stack to the start destination of the current navController graph
+        navController.popBackStack(
+            navController.graph.startDestination, false
+        )
+    }
+}
+
+private fun detachNavHostFragment(
+    fragmentManager: FragmentManager,
+    navHostFragment: NavHostFragment
+) {
+    fragmentManager.beginTransaction()
+        .detach(navHostFragment)
+        .commitNow()
+}
+
+private fun attachNavHostFragment(
+    fragmentManager: FragmentManager,
+    navHostFragment: NavHostFragment,
+    isPrimaryNavFragment: Boolean
+) {
+    fragmentManager.beginTransaction()
+        .attach(navHostFragment)
+        .apply {
+            if (isPrimaryNavFragment) {
+                setPrimaryNavigationFragment(navHostFragment)
+            }
+        }
+        .commitNow()
+}
+
+private fun AppCompatActivity.obtainNavHostFragment(
+    @NavigationRes graphResId: Int,
+    startDestination: Int,
+    fragmentManager: FragmentManager,
+    fragmentTag: String,
+    containerId: Int
+): NavHostFragment {
+    // If the Nav Host fragment exists, return it
+    val existingFragment = fragmentManager.findFragmentByTag(fragmentTag) as NavHostFragment?
+    existingFragment?.let { return it }
+
+    // This is required for explicit deep links, aka push notifications.
+    // We need to remove temporally the intent of the activity because when the graph is created,
+    // NavController::onGraphCreated calls internally NavController::handleDeepLink which adds, based on the intent,
+    // the screen destination from the deep link, specifically it relies in the key bundle
+    // NavController::KEY_DEEP_LINK_IDS, but because this is an internal property we can't rely on the fact that
+    // this won't change, thus we remove the complete intent during the creating execution and we put back
+    // the intent to the activity later on.
+    val originalIntent = intent
+    intent = Intent()
+
+    // Calling NavHostFragment() will work and that ould avoid creating an empty fragment as the start destination,
+    // but it starts to corrupt the back stack when Don't keep activities is enabled, which is avoided by
+    // calling NavHostFragment.create(graphResId) and setting as the start destination of the graph -in the xml
+    // declaration- an empty fragment.
+    val navHostFragment = NavHostFragment.create(graphResId)
+    fragmentManager.beginTransaction()
+        .add(containerId, navHostFragment, fragmentTag)
+        .commitNow()
+
+    navHostFragment.navController.graph = navHostFragment.navController.navInflater.inflate(graphResId)
+        .also { graph -> graph.startDestination = startDestination }
+
+    intent = originalIntent
+
+    return navHostFragment
+}
+
+private fun FragmentManager.isOnBackStack(backStackName: String): Boolean {
+    val backStackCount = backStackEntryCount
+    for (index in 0 until backStackCount) {
+        if (getBackStackEntryAt(index).name == backStackName) {
+            return true
+        }
+    }
+    return false
+}
+
+private fun getFragmentTag(index: Int) = "bottomNavigation#$index"
